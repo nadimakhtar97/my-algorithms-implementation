cbb771d2dfd910d92de5c46d82a0dfa8ff8962a0
Nadim Akhtar <48604201+nadimakhtar97@users.noreply.github.com>
2022-07-12 19:14:59 +0530

    input.cpp

---
+++ input.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define f first
+#define s second
+#define pb push_back
+#define MAX 1000000007
+#define MOD 998244353
+#define endl "\n"
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+#define watch2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+using namespace std;
+
+int32_t main()
+{
+
+    FIO;
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+
+
+    
+}
